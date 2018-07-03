853f30b6044144fa53094e83d6263b239ea8339f
jtbudd451 <buddwebsolutions@gmail.com>
2018-07-03 16:15:33 -0400

    Initial functionality

---
+++ js/dm.js
@@ -0,0 +1,14 @@
+var i = 6;
+var DIE_MIN = 1;
+var Alltext = " ";
+
+// Roll a # sided die
+function roll_die(max){
+	console.log("Rolling die " + max);
+	return Math.floor(Math.random() * (max - DIE_MIN)) + DIE_MIN;
+}
+
+function add_text(text) {
+	Alltext += text + "\n";
+	document.getElementById("story").value = Alltext
+}
